24b3a831066e8d8b8cdeefa20ffc2da5a5e3e4b6
jacobmasvidal <jmasvidal@patreon.com>
2024-10-28 12:44:18 -0400

    Added timeMin variable and additional logic to more accurately return results.

--- GAM/Return All Regular Calendar Events.sh
+++ GAM/Return All Regular Calendar Events.sh
@@ -15,18 +15,18 @@ options=("Today" "Custom")
 
 # The function for select Today.
 function today {
-	date=$(date '+%Y-%m-%d')
+  date=$(date '+%Y-%m-%d')
 	yesterday=$(date -v -1d '+%Y-%m-%d')
 	tomorrow=$(date -v +1d '+%Y-%m-%d')
-	$gam config auto_batch_min 1 redirect csv ./$date.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday && $gam user $owner add drivefile localfile $date.csv
+	$gam config auto_batch_min 1 redirect csv ./$date.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday timemin $date"T00:00:00+00:00" && $gam user $owner add drivefile localfile $date.csv
 }
 
 # The function for selecting Custom.
 function custom {
   read -p 'Enter the date before the target date in yyyy-mm-dd format (2024-01-01 for target 2024-01-02): ' yesterday
   read -p 'Enter the date after the target date in yyyy-mm-dd format (2024-01-03 for target 2024-01-02): ' tomorrow
-  $gam config auto_batch_min 1 redirect csv ./$yesterday-and-$tomorrow.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday && $gam user $owner add drivefile localfile $yesterday-and-$tomorrow.csv
-
+  date=$(date -j -f "%Y-%m-%d" "$(date -j -v+1d -f "%Y-%m-%d" $yesterday "+%Y-%m-%d")" "+%Y-%m-%d")
+  $gam config auto_batch_min 1 redirect csv ./$yesterday-and-$tomorrow.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday timemin $date"T00:00:00+00:00" && $gam user $owner add drivefile localfile $yesterday-and-$tomorrow.csv
 }
 
 # Runs the selected option and associated function.
