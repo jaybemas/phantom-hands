6d9afb7158eb96ed9b4ee45c0558fdc443d765e4
jacobmasvidal <jmasvidal@patreon.com>
2024-10-29 10:50:49 -0400

    Added function for reporting on a specific date. Fixed quote error.

--- GAM/Return All Regular Calendar Events.sh
+++ GAM/Return All Regular Calendar Events.sh
@@ -7,13 +7,14 @@
 gam="$HOME/bin/gamadv-xtd3/gam"
 
 # The Google Drive account that should own the resulting csv upload.
-owner="test@yourDomain.com
+owner="test@yourDomain.com"
 
-# Function selection.
+# Function selection and creation of date variable.
 PS3="Select the Calendar Date: "
-options=("Today" "Custom")
+options=("Today" "Specific Day" "Custom Range")
 
-# The function for select Today.
+
+# The function for selecting Today.
 function today {
   date=$(date '+%Y-%m-%d')
 	yesterday=$(date -v -1d '+%Y-%m-%d')
@@ -21,12 +22,21 @@ function today {
 	$gam config auto_batch_min 1 redirect csv ./$date.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday timemin $date"T00:00:00+00:00" && $gam user $owner add drivefile localfile $date.csv
 }
 
-# The function for selecting Custom.
-function custom {
+# The function for selecting Custom Range.
+function customRange {
   read -p 'Enter the date before the target date in yyyy-mm-dd format (2024-01-01 for target 2024-01-02): ' yesterday
   read -p 'Enter the date after the target date in yyyy-mm-dd format (2024-01-03 for target 2024-01-02): ' tomorrow
-  date=$(date -j -f "%Y-%m-%d" "$(date -j -v+1d -f "%Y-%m-%d" $yesterday "+%Y-%m-%d")" "+%Y-%m-%d")
+  date=$(date -j -f "%Y-%m-%d" "$(date -j -v +1d -f "%Y-%m-%d" $yesterday "+%Y-%m-%d")" "+%Y-%m-%d")
   $gam config auto_batch_min 1 redirect csv ./$yesterday-and-$tomorrow.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday timemin $date"T00:00:00+00:00" && $gam user $owner add drivefile localfile $yesterday-and-$tomorrow.csv
+
+}
+
+# The function for selecting Specific Day.
+function specificDay {
+  read -p 'Enter the specific date in yyyy-mm-dd format (2024-01-02): ' targetDay
+  yesterday=$(date -j -f "%Y-%m-%d" "$(date -j -v -1d -f "%Y-%m-%d" $targetDay "+%Y-%m-%d")" "+%Y-%m-%d")
+  tomorrow=$(date -j -f "%Y-%m-%d" "$(date -j -v +1d -f "%Y-%m-%d" $targetDay "+%Y-%m-%d")" "+%Y-%m-%d")
+  $gam config auto_batch_min 1 redirect csv ./$targetDay.csv multiprocess all users print events singleevents eventtype default before $tomorrow after $yesterday timemin $targetDay"T00:00:00+00:00" && $gam user $owner add drivefile localfile $targetDay.csv
 }
 
 # Runs the selected option and associated function.
@@ -36,8 +46,12 @@ select option in "${options[@]}"; do
 		  today
 		  break
 		  ;;
+		"Specific Day")
+		  specificDay
+		  break
+		  ;;
 	  "Custom")
-	    custom
+	    customRange
 	    break
 	    ;;
   esac
